67d3dbffe0a536285b38e8d372958ad7c2493c2c
Josue Lubaki <josuelubaki@gmail.com>
2021-05-05 02:11:12 -0400

    base du projet

--- Queens-On-Board/Form1.cs
+++ Queens-On-Board/Form1.cs
@@ -15,6 +15,102 @@ namespace Queens_On_Board
         public Form1()
         {
             InitializeComponent();
+            ResoudreEchequier(8);
         }
+
+        private static int nombreK_prometteur = 0;
+
+        /**
+        * Methode qui permet d'afficher toutes les cases de l'échiquier
+        * @return void
+        */
+        void AfficherSolution(Matrice board)
+        {
+            for (int i = 0; i < board.ColSize; i++)
+            {
+                for (int j = 0; j < board.ColSize; j++)
+                    Console.Write(" " + board[i, j]
+                                      + " ");
+                Console.WriteLine();
+            }
+        }
+
+        
+        /**
+         * Methode qui vérifie si la reine est en sécurité (hors de danger) sur la ligne, colonne et diagonale
+         * @return boolean
+         */
+        bool estEnSecurite(Matrice board, int row, int col)
+        {
+            int i, j;
+
+            /* vérifier les colonnes pour une ligne fixe */
+            for (i = 0; i < col; i++)
+                if (board[row, i] == 1)
+                    return false;
+
+            /* Vérifier la diagonale gauche supérieur de l'échiquier */
+            for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+                if (board[i, j] == 1)
+                    return false;
+
+            /* Vérifier la diagonale gauche inférieure de l'échiquier */
+            for (i = row, j = col; j >= 0 && i < board.ColSize; i++, j--)
+                if (board[i, j] == 1)
+                    return false;
+
+            return true;
+        }
+
+        /* Methode qui résout le problèée de la reine d'une manière recursive */
+        bool ResolutionPartielle(Matrice board, int col)
+        {
+            /* Si toutes les reines sont placées, on retourne vrai */
+            if (col >= board.RowSize)
+                return true;
+
+            /* on essaye de placer une reine sur toutes les lignes une par une */
+            for (int i = 0; i < board.RowSize; i++)
+            {
+               
+                //verifie si une reine peut etre placee dans la ligne i(++) d'une colonne (col) bien precise sans conflit 
+                if (estEnSecurite(board, i, col))
+                {
+                    //mettre la reine a la position  board[i,col] 
+                    board[i, col] = 1;
+                    nombreK_prometteur++;
+
+                    //passer a la colone suivante et refaire l'operation pour inserer la reine  
+                    if (ResolutionPartielle(board, col + 1) == true)
+                        return true;
+
+                    /* si placer une reine board[i,col]
+                    ne mene pas a une solution alors on enleve la reine du board[i,col] */
+                    board[i, col] = 0; // BACKTRACK
+                }
+            }
+
+
+            //Si la reine ne peut  etre placée dans aucune ligne dans cette colone , on retourne faux
+           
+            return false;
+        }
+
+        
+        bool ResoudreEchequier(int N)
+        {
+            Matrice matrice = new Matrice(N);
+
+            if (ResolutionPartielle(matrice, 0) == false)
+            {
+                Console.Write("La Solution n'existe pas !");
+                return false;
+            }
+
+            AfficherSolution(matrice);
+            Console.Write("Voici le nombre de k-prometteurs produits avant : " + nombreK_prometteur);
+            return true;
+        }
+
     }
 }
---
+++ Queens-On-Board/Matrice.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queens_On_Board
+{
+    class Matrice
+    {
+        /**********************************************************************************************/
+        /***************                VARIABLES INSTANCES & CONSTRUCTEUR              ***************/
+        /**********************************************************************************************/
+        private int[,] data;
+        public Matrice(int dimension)
+        {
+            this.data = new int[dimension, dimension];
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    Data[i, j] = 0;
+                }
+            }
+        }
+        public int[,] Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+
+        /**********************************************************************************************/
+        /***************                    DIMENSION DE LA MATRICE                     ***************/
+        /**********************************************************************************************/
+        /** Obtenir la Taille de la Matrice : Ligne et Colonne 
+         *  @param RowSize : Nombre des lignes contenues dans la matrice
+         *  @param ColSize : Nombre des Colonnes contenues dans la matrice 
+         *  
+         *  @return int    */
+        public int RowSize
+        {
+            get { return data.GetLength(0); }
+        }
+        public int ColSize
+        {
+            get { return data.GetLength(1); }
+        }
+
+        /**********************************************************************************************/
+        /***************                MANIPULATION DES DONNEES DE LA MATRICE          ***************/
+        /**********************************************************************************************/
+        /** Obtenir un élement en particulier du Tableau 
+         *  1er Paramètre : Correspond au numero de Ligne 
+         *  2ème Paramètre : Correspond au numero de la Colonne 
+         *  
+         *  @return double  
+         */
+        public int GetElement(int ligne, int colonne)
+        {
+            return Data[ligne, colonne];
+        }
+
+        /** Modifier un élement en particulier du Tableau
+         *  1er Paramètre : Correspond au numero de Ligne 
+         *  2ème Paramètre : Correspond au numero de la Colonne 
+         *  3ième Paramètre : Correspond à la nouvelle Valeur à Insérer 
+         *  
+         *  @return void    */
+        public void SetElement(int ligne, int colonne, int value)
+        {
+            Data[ligne, colonne] = value;
+        }
+
+
+
+        /**********************************************************************************************/
+        /****************                FORMAT DE SORTIE                               ***************/
+        /**********************************************************************************************/
+        /** Indexeur de la matrice 
+         *  1er Paramètre : Correspond au numero de Ligne 
+         *  2ème Paramètre : Correspond au numero de la Colonne 
+         *  
+         *  @return double  */
+        public int this[int i, int j]
+        {
+            get { return data[i, j]; }
+            set { data[i, j] = value; }
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            for (int i = 0; i < RowSize; i++)
+            {
+                for (int j = 0; j < ColSize; j++)
+                {
+                    output += this[i, j];
+                }
+            }
+            return output;
+        }
+    }
+
+}
